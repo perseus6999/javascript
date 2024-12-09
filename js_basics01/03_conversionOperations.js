98cd38eac176ee277e318f3a15d192d152419453
perseus6999 <154442624+perseus6999@users.noreply.github.com>
2024-12-09 11:37:23 +0000

    conversions and operations

---
+++ js_basics01/03_conversionOperations.js
@@ -0,0 +1,39 @@
+
+// CONVERSION ->>>>>>>
+
+// string to number 
+// "33" -> number 
+// "33avs" -> NaN
+
+// let score = "22aa" 
+
+// console.log(typeof score)  
+
+// score = Number(score) // we can convert it into number
+// console.log(typeof score)  
+// console.log(score)
+
+// score = Boolean(score) // we can convert it into boolean
+
+// console.log(typeof score)
+// console.log(score)
+
+// ""(string) -> 0(boolean)
+// "ababhaw"(stirng) -> 1(boolean)
+
+// OPERATIONS ->>>>>>>
+
+
+console.log("1" + 2) 
+console.log(1 + "2") 
+console.log(1 + 2 + "3" ) // here as first addition operation present so that operation will take place
+console.log("1" + 2 + 3) // here as first string operation present so that operation will take place
+
+
+console.log(true)
+console.log(+true)
+// console.log(true+) error
+ 
+console.log("")
+console.log(+"")
+//console.log(""+) error
