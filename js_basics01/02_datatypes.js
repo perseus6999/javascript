d6109ff7d503d823981e860353bc1936fb45bfc1
perseus6999 <154442624+perseus6999@users.noreply.github.com>
2024-12-09 10:56:40 +0000

    datatypes basics

---
+++ js_basics01/02_datatypes.js
@@ -0,0 +1,15 @@
+
+// primitive datatypes
+
+// number -> upto  2e53
+// bigint -> for large numbers 
+// string  
+// boolean 
+// null -> standalone value
+// undefined -> 
+// symbol -> unique 
+
+// object 
+
+console.log(typeof undefined) 
+console.log(typeof null)
